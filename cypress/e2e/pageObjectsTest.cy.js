397566b446a2803a6c93e74648771548dff68023
Yuri Mielli <yumielllli@gmail.com>
2025-12-17 17:42:44 +0100

    adicionando profile.page e algumas modificaçoes

--- cypress/e2e/pageObjectsTest.cy.js
+++ cypress/e2e/pageObjectsTest.cy.js
@@ -4,6 +4,7 @@
  const {profileCreate} =  require('../support/pages/home.page')
  const {signUpProfile} = require('../support/pages/sign.up.page')
  const {firstName, lastName, phoneNumber, email, password, reEnterPassword} = require('../fixtures/data.json')
+ const {profilePage} = require('../support/pages/profile.page')
 
 describe("Teste de criação de conta", () =>{
 
@@ -21,5 +22,6 @@ describe("Teste de criação de conta", () =>{
     cy.wait(2000)
     createAccount.newAccount(firstName, lastName, phoneNumber, email, password, reEnterPassword)
     profileCreate.newProfile('Account')
+    profilePage.customerName().should('exist')
     })
 })
