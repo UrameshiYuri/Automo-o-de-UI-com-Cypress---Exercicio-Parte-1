f5a1274a7e83a7d852129feb3974987794763f2b
Yuri Mielli <yumielllli@gmail.com>
2025-12-18 23:06:03 +0100

    mudanças compra.produto

--- cypress/e2e/lojaEbacTest.cy.js
+++ cypress/e2e/lojaEbacTest.cy.js
@@ -9,6 +9,7 @@ describe('fluxo de checkout do carrinho com AppActions', () => {
     it('deve fazer checkout com sucesso na loja EBAC', () => {
         
         cy.login(email, senha)
+        cy.wait(2000)
         cy.compra(produto)
     })
 
